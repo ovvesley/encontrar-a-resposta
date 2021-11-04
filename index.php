e36a8fb7fd08fd96259b266b373f7e85a3a8eb26
Wesley Ferreira <ovvesley@protonmail.com>
2021-11-03 21:46:22 -0300

    Crawler: answer

--- index.php
+++ index.php
@@ -1,25 +1,113 @@
 <?php
 
+function getUrl()
+{
+    $url = "http://applicant-test.us-east-1.elasticbeanstalk.com/";
+    return $url;
+}
 
-$url_initial_page = "http://applicant-test.us-east-1.elasticbeanstalk.com/";
+function getCookiesFromResponse($http_response_header)
+{
+    $cookies = null;
+    if (preg_match_all('/Set-Cookie:[\s]([^;]+)/', $http_response_header[3], $matches)) {
+        $cookies = $matches[1][0];
+    }
+    
+    return $cookies;
+}
 
-$raw_html_initial_page = file_get_contents($url_initial_page);
+function getTokenInitialPage($page)
+{
+    $token_id = 'token';
+    $token_element = $page->getElementById($token_id);
+    $token_value = $token_element->getAttribute('value');
+    return $token_value;
+}
 
-$page_html_initial_page = new DOMDocument();
-$page_html_initial_page->loadHTML($raw_html_initial_page);
+function replacementToken($token)
+{
+    $new_token_value = null;
+    $replacement = json_decode('{"0":"9","1":"8","2":"7","3":"6","4":"5","5":"4","6":"3","7":"2","8":"1","9":"0","a":"z","b":"y","c":"x","d":"w","e":"v","f":"u","g":"t","h":"s","i":"r","j":"q","k":"p","l":"o","m":"n","n":"m","o":"l","p":"k","q":"j","r":"i","s":"h","t":"g","u":"f","v":"e","w":"d","x":"c","y":"b","z":"a"}', true);
 
-$token_id = 'token';
-$token_element = $page_html_initial_page->getElementById($token_id);
-$token_value = $token_element->getAttribute('value');
+    for ($i = 0; $i < strlen($token); $i++) {
+        $new_token_value .= $replacement[$token[$i]];
+    }
+    return $new_token_value;
+}
 
-$data = ['token' => $token_value];
+function handleDataToRequest($data)
+{
+    return http_build_query($data);
+}
 
-$response_post_request_initial_page = file_get_contents($url_initial_page, false, stream_context_create([
-    'http' => [
-        'method' => 'POST',
-        'header'  => "Content-type: application/x-www-form-urlencoded",
-        'content' => http_build_query($data)
-    ]
-]));
 
-var_dump($response_post_request_initial_page);
+function handleInitialPage()
+{
+
+    $urlInitialPage = getUrl();
+
+    $rawHtmlInitialPage = file_get_contents($urlInitialPage);
+    $cookies = getCookiesFromResponse($http_response_header);
+
+    $pageHtmlInitial = new DOMDocument();
+    $pageHtmlInitial->loadHTML($rawHtmlInitialPage);
+
+    $token  = getTokenInitialPage($pageHtmlInitial);
+
+    return [
+        'token' => $token,
+        'cookies' => $cookies
+    ];
+}
+
+function handleAnswerResponse($rawHtml)
+{
+    $pageHtmlInitial = new DOMDocument();
+    $pageHtmlInitial->loadHTML($rawHtml);
+
+    $answer = $pageHtmlInitial->getElementById("answer")->textContent;
+
+    return $answer;
+}
+
+function handleAnswerPage($token, $cookies)
+{
+    $url = getUrl();
+
+    $data = handleDataToRequest(['token' => $token]);
+
+
+    $response = file_get_contents($url, false, stream_context_create([
+        'http' => [
+            'method' => 'POST',
+            'header'  => [
+                "Cookie: {$cookies}",
+                'Referer: http://applicant-test.us-east-1.elasticbeanstalk.com/',
+            ],
+            'content' => $data,
+        ]
+    ]));
+
+    $answer = handleAnswerResponse($response);
+
+    return $answer;
+}
+
+
+function index()
+{
+
+    $initialPage = handleInitialPage();
+
+    $tokenInitialPage = $initialPage['token'];
+    $cookies = $initialPage['cookies'];
+
+    $newToken = replacementToken($tokenInitialPage);
+
+    $answer = handleAnswerPage($newToken, $cookies);
+
+    error_log("Resposta: {$answer}");
+}
+
+
+index();
